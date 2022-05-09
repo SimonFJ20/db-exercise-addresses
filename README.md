d5201b5645e8874535639316398b6f463c8fc06e
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-05-09 15:23:23 +0200

    more splittin

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 Address be like
 
 ```
-id, status, oprettet, aendret, husnr, etage, doer, supplerendebynavn, matrikelnr, esrejendomsnr, etrs89koordinat_oest, etrs89koordinat_nord, wgs84koordinat_bredde, wgs84koordinat_laengde, noejagtighed, kilde, tekniskstandard, tekstretning, ddkn_m100, ddkn_km1, ddkn_km10, adressepunktaendringsdato, adgangsadresseid, adgangsadresse_status, adgangsadresse_oprettet, adgangsadresse_aendret, kvhx, jordstykke_matrikelnr, jordstykke_esrejendomsnr, kvh, hoejde, adgangspunktid, vejpunkt_id, vejpunkt_noejagtighed, vejpunkt_tekniskstandard, vejpunkt_x, vejpunkt_y, brofast, supplerendebynavn_dagi_id, vejpunkt_aendret, ikrafttraedelse, nedlagt, adgangsadresse_ikrafttraedelse, adgangsadresse_nedlagt, adgangsadresse_darstatus, darstatus, betegnelse
+id, status, oprettet, aendret, husnr, etage, doer, matrikelnr, esrejendomsnr, etrs89koordinat_oest, etrs89koordinat_nord, wgs84koordinat_bredde, wgs84koordinat_laengde, noejagtighed, kilde, tekniskstandard, tekstretning, ddkn_m100, ddkn_km1, ddkn_km10, adressepunktaendringsdato, adgangsadresseid, adgangsadresse_status, adgangsadresse_oprettet, adgangsadresse_aendret, kvhx, kvh, hoejde, adgangspunktid, brofast, ikrafttraedelse, nedlagt, adgangsadresse_ikrafttraedelse, adgangsadresse_nedlagt, adgangsadresse_darstatus, darstatus, betegnelse
 ```
--- make_sub_tables.sql
+++ make_sub_tables.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE `vejnavn` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `kode` smallint NOT NULL,
-  `navn` varchar(23) NOT NULL,
-  `adresseringsnavn` varchar(20) NOT NULL
+CREATE TABLE `vej` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `kode` smallint NOT NULL,
+    `navn` varchar(23) NOT NULL,
+    `adresseringsnavn` varchar(20) NOT NULL
 );
 
-INSERT INTO `vejnavn` (`kode`, `navn`, `adresseringsnavn`)
+INSERT INTO `vej` (`kode`, `navn`, `adresseringsnavn`)
     SELECT DISTINCT
         `vejkode` AS `kode`,
         `vejnavn` AS `navn`,
@@ -14,9 +14,9 @@ INSERT INTO `vejnavn` (`kode`, `navn`, `adresseringsnavn`)
     FROM `raw`
 
 CREATE TABLE `postnr` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nr` int NOT NULL,
-  `navn` varchar(12) NOT NULL
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nr` int NOT NULL,
+    `navn` varchar(12) NOT NULL
 );
 
 INSERT INTO `postnr` (`nr`, `navn`)
@@ -25,20 +25,37 @@ INSERT INTO `postnr` (`nr`, `navn`)
         `postnrnavn` AS `navn`
     FROM `raw`
 
-CREATE TABLE `supplerendebynavn` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY;
-  `navn` varchar(14) NULL
+CREATE TABLE `stormodtagerpostnr` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nr` int NOT NULL,
+    `navn` varchar(12) NOT NULL
 );
 
-INSERT INTO `supplerendebynavn` (`navn`)
+INSERT INTO `postnr` (`nr`, `navn`)
     SELECT DISTINCT
-        `supplerendebynavn` AS `navn`
+        `stormodtagerpostnr` AS `nr`,
+        `stormodtagerpostnrnavn` AS `navn`
     FROM `raw`
+    WHERE NOT ISNULL(`stormodtagerpostnr`)
+    AND NOT ISNULL(`stormodtagerpostnrnavn`)
+
+CREATE TABLE `supplerendebynavn` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `navn` varchar(14) NOT NULL,
+    `dagi_id` int NOT NULL
+);
+
+INSERT INTO `supplerendebynavn` (`navn`, `dagi_id`)
+    SELECT DISTINCT
+        `supplerendebynavn` AS `navn`,
+        `supplerendebynavn_dagi_id` AS `dagi_id`
+    FROM `raw`
+    WHERE NOT ISNULL(`supplerendebynavn`)
 
 CREATE TABLE `kommune` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `kode` int NOT NULL,
-  `navn` varchar(6) NOT NULL
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `kode` int NOT NULL,
+    `navn` varchar(6) NOT NULL
 );
 
 INSERT INTO `kommune` (`kode`, `navn`)
@@ -48,9 +65,9 @@ INSERT INTO `kommune` (`kode`, `navn`)
     FROM `raw`
 
 CREATE TABLE `ejerlav` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `kode` int NOT NULL,
-  `navn` varchar(34) NOT NULL
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `kode` int NOT NULL,
+    `navn` varchar(34) NOT NULL
 );
 
 INSERT INTO `ejerlav` (`kode`, `navn`)
@@ -211,4 +228,71 @@ CREATE TABLE `vejpunkt_kilde` (
 INSERT INTO `vejpunkt_kilde` (`navn`)
     SELECT DISTINCT
         `vejpunkt_kilde` AS `navn`
+    FROM `raw`
+
+CREATE TABLE `vejpunkt` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `guid` varchar(36) NOT NULL,
+    `kilde` int NOT NULL,
+    `noejagtighed` char(1) NOT NULL,
+    `tekniskstandard` varchar(2) NOT NULL,
+    FOREIGN KEY (`kilde`) REFERENCES `vejpunkt_kilde` (`id`),
+    `x` double NOT NULL,
+    `y` double NOT NULL AFTER `x`,
+    `aendret` varchar(26) NOT NULL
+);
+
+INSERT INTO `vejpunkt` (`guid`, `noejagtighed`, `tekniskstandard`, `kilde`, `x`, `y`, `aendret`)
+    SELECT DISTINCT
+        `vejpunkt_id` AS guid,
+        `vejpunkt_noejagtighed` AS `noejagtighed`,
+        `vejpunkt_tekniskstandard` AS `tekniskstandard`,
+        `vejpunkt_kilde`.`id` as `kilde`,
+        `vejpunkt_x` AS `x`,
+        `vejpunkt_y` AS `y`,
+        `vejpunkt_aendret` AS `aendret`
+    FROM `raw`
+    LEFT JOIN `vejpunkt_kilde`
+    ON `raw`.`vejpunkt_kilde` = `vejpunkt_kilde`.`navn`
+
+CREATE TABLE `jordstykke` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `matrikelnr` varchar(15) NOT NULL,
+  `esrejendomsnr` int NOT NULL,
+  `ejerlav` int NOT NULL,
+  FOREIGN KEY (`ejerlav`) REFERENCES `jordstykke_ejerlav` (`id`)
+);
+
+INSERT INTO `jordstykke` (`matrikelnr`, `esrejendomsnr`, `ejerlav`)
+    SELECT DISTINCT
+        `jordstykke_matrikelnr` AS `matrikelnr`,
+        `jordstykke_esrejendomsnr` AS `esrejendomsnr`,
+        `jordstykke_ejerlav`.`id` AS `ejerlav`
+    FROM `raw`
+    LEFT JOIN `jordstykke_ejerlav`
+    ON `jordstykke_ejerlav`.`kode` = `jordstykke_ejerlavkode`
+    WHERE
+        NOT ISNULL(`jordstykke_matrikelnr`)
+        AND NOT ISNULL(`jordstykke_esrejendomsnr`)
+
+CREATE TABLE `adgangsadresse` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `guid` varchar(36) NOT NULL,
+    `status` int NOT NULL,
+    `oprettet` varchar(23) NOT NULL,
+    `aendret` varchar(23) NOT NULL,
+    `ikrafttraedelse` varchar(23) COLLATE 'utf8mb4_0900_ai_ci' NULL,
+    `nedlagt` varchar(23) COLLATE 'utf8mb4_0900_ai_ci' NULL AFTER `ikrafttraedelse`,
+    `darstatus` int NOT NULL AFTER `nedlagt`
+);
+
+INSERT INTO `adgangsadresse` (`guid`, `status`, `oprettet`, `aendret`, `ikrafttraedelse`, `nedlagt`, `darstatus`)
+    SELECT DISTINCT
+        `adgangsadresseid` AS `guid`,
+        `adgangsadresse_status` AS `status`,
+        `adgangsadresse_oprettet` AS `oprettet`,
+        `adgangsadresse_aendret` AS `aendret`,
+        `adgangsadresse_ikrafttraedelse` AS `ikrafttraedelse`,
+        `adgangsadresse_nedlagt` AS `nedlagt`,
+        `adgangsadresse_darstatus` AS `darstatus`
     FROM `raw`
